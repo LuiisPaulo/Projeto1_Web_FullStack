2651b57da68c6eca5ee85fb7fa09b3da3632868e
LuisPaulo <luispaulolopesg@gmail.com>
2025-05-08 08:21:28 -0300

    "Fix: Primary Commit"

---
+++ blackjack/src/contexts/Game.jsx
@@ -0,0 +1,12 @@
+const start = {
+    deckId: null,
+    playerDeacks: [],
+    dealerDeack: [],
+    status_game: 'not_started',
+    result: null
+}
+
+function DynamicGame(state, action){
+    
+
+}
